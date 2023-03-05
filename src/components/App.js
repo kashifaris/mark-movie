067de33c6de14d9b1aaf745eb3b14f548de79867
kashif khan <mokashifkhan21@gmail.com>
2023-03-05 17:41:03 +0530

    added store

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,26 @@ import React from 'react';
 import {data} from '../../../movie-app/src/data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
-function App() {
+class App extends React.Component {
+  componentDidMount(){
+    console.log("component mounted");
+    const {store}=this.props;
+
+    store.subscribe(()=>{
+      console.log(" subscribe called"); 
+      this.forceUpdate();
+    });
+
+    console.log("dispatch finished");
+
+    store.dispatch(addMovies(data));
+    
+  }
+  render(){
+  const movies=this.props.store.getState();
+  console.log("render");
   return (
     <div className="App">
       <Navbar/>
@@ -13,13 +31,14 @@ function App() {
         <div className="tab">Favourites</div>
         </div>
         <div className="list">
-          {data.map(movie=>(
-            <MovieCard movie={movie}/>
+          {movies.map((movie, index)=>(
+            <MovieCard movie={movie} key={index}/>
           ))}
         </div>
          </div>
     </div>
   );
+   }
 }
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import {ADD_MOVIES} from '../actions';
+export default function movies (state=[],action){
+    if(action.type===ADD_MOVIES){
+        return action.movies;
+    }
+    return state;
+}
